1829c677e70c04400490ac254ebfea319f8e7cef
Edivan Sousa <94314273+sousaep1974@users.noreply.github.com>
2022-05-15 02:54:05 -0300

    Atualizar

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import SmartCounter from '../Smartcounter/Smartcounter';
+
+
+function App() {
+  return (
+    <>
+<h1>Hello World!</h1>
+<SmartCounter/>
+</>
+  );
+}
+
+export default App;
